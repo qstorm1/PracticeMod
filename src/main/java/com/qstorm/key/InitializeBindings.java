1ed89cb59d9536f62f31a58aeaeb0de4108f1504
Arya <qstorm421@gmail.com>
2026-08-18 17:29:48 -0700

    Fixed bug where when you fire laser and press other key, laser stops fireing (also through that fixed mouse button firing issue)

--- src/main/java/com/qstorm/key/InitializeBindings.java
+++ src/main/java/com/qstorm/key/InitializeBindings.java
@@ -6,33 +6,35 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.minecraft.client.KeyMapping;
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.Identifier;
 
 public class InitializeBindings {
-    public static KeyMapping laser;
-    public static KeyMapping domain;
+    public static KeyMapping laserKey;
+    public static KeyMapping domainKey;
+
+    private static boolean prevState = false;
     public static void init(){
         KeyMapping.Category CATEGORY = KeyMapping.Category.register(Identifier.fromNamespaceAndPath(PracticeMod.MOD_ID,"custom_mod_controls"));
-        laser = KeyBindingHelper.registerKeyBinding(
+        laserKey = KeyBindingHelper.registerKeyBinding(
                 new KeyMapping(
-                        "key.laser", InputConstants.MOUSE_BUTTON_LEFT,CATEGORY
+                        "laser", InputConstants.MOUSE_BUTTON_RIGHT,CATEGORY
                 )
         );
 
 
-        domain = KeyBindingHelper.registerKeyBinding(
+        domainKey = KeyBindingHelper.registerKeyBinding(
                 new KeyMapping(
-                        "key.domain", InputConstants.KEY_SEMICOLON,CATEGORY
+                        "domain", InputConstants.KEY_SEMICOLON,CATEGORY
                 )
         );
+
 
 
 
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while(laser.consumeClick()){
-                if(client.player!=null){
+            if(laserKey.isDown()){
+                if(client.player!=null) {
                     ClientPlayNetworking.send(new HandleKeybinds.LaserKeyServer());
                 }
             }
