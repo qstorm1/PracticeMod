2b8ceb40573d201089643adc67662c5fca9d8229
Arya <qstorm421@gmail.com>
2026-09-14 01:47:23 -0700

    Fixed stuff related to the innate

--- src/main/java/com/qstorm/mixin/LLInnateProjectile.java
+++ src/main/java/com/qstorm/mixin/LLInnateProjectile.java
@@ -1,8 +1,15 @@
 package com.qstorm.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.qstorm.powers.cursedtechnique.Sorcery;
 import com.qstorm.powers.cursedtechnique.limitless.Limitless;
+import com.qstorm.powers.cursedtechnique.limitless.power.LimitlessInnate;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,14 +17,28 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Projectile.class)
+import java.util.Optional;
+import java.util.function.Predicate;
+
+@Mixin(ProjectileUtil.class)
 public class LLInnateProjectile {
 
-    @Inject(method="canHitEntity",at= @At("HEAD"),cancellable = true)
-    public void canHitEntity(Entity target, CallbackInfoReturnable<Boolean> cir){
-        if((Object)target instanceof Limitless ls &&ls.innateOn){
-            ((Entity)(Object)this).setDeltaMovement(Vec3.ZERO);
-            ((Entity)(Object)this).setNoGravity(true);
+
+    @Inject(
+        method = "getEntityHitResult(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/Entity;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/Vec3;Lnet/minecraft/world/phys/AABB;Ljava/util/function/Predicate;F)Lnet/minecraft/world/phys/EntityHitResult;",
+        at=@At(value="RETURN",ordinal=1),cancellable = true)
+    private static void getEntityHitResult(Level level, Entity projectile, Vec3 startVec, Vec3 endVec, AABB boundingBox, Predicate<Entity> filter, float inflationAmount,
+                                       CallbackInfoReturnable<EntityHitResult> cir, @Local(ordinal = 0, argsOnly = true) Entity entity){
+        if(Sorcery.sorcerers.get(entity.getUUID()) != null){
+            //if the target is a sorcerer
+            Sorcery.sorcerers.get(entity.getUUID()).abilities.forEach((ability -> {
+                if (ability instanceof LimitlessInnate li && li.isTicked) {
+                    cir.setReturnValue(null);
+//                    if(!projectile.getTags().contains(LimitlessInnate.TAG))
+//                        projectile.addTag(LimitlessInnate.TAG);
+                }
+            }));
+
         }
 
     }
